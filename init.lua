cd2f9995a2b71172859f601fcf7453f613d0fadb
nibon7 <nibon7@163.com>
2025-09-22 19:51:09 +0800

    feat(treesitter): Enable additional treesitter languages

--- init.lua
+++ init.lua
@@ -909,7 +909,29 @@ require('lazy').setup({
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
       -- ensure basic parser are installed
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local parsers = {
+        'bash',
+        'c',
+        'cpp',
+        'cmake',
+        'diff',
+        'git_config',
+        'html',
+        'json',
+        'kconfig',
+        'lua',
+        'luadoc',
+        'make',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'python',
+        'rust',
+        'toml',
+        'vim',
+        'vimdoc',
+        'yaml',
+      }
       require('nvim-treesitter').install(parsers)
 
       ---@param buf integer
