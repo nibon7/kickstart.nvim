88ccddb4f17d2a06dd816a3a7340ad86b0030d87
nibon7 <nibon7@163.com>
2025-10-23 16:42:41 +0800

    feat(keymap): Remap <leader>hn/<leadere>hN to jump to next/prev git change

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -15,7 +15,7 @@ require('gitsigns').setup {
     end
 
     -- Navigation
-    map('n', ']c', function()
+    map('n', '<leader>hn', function()
       if vim.wo.diff then
         vim.cmd.normal { ']c', bang = true }
       else
@@ -23,7 +23,7 @@ require('gitsigns').setup {
       end
     end, { desc = 'Jump to next git [c]hange' })
 
-    map('n', '[c', function()
+    map('n', '<leader>hN', function()
       if vim.wo.diff then
         vim.cmd.normal { '[c', bang = true }
       else
