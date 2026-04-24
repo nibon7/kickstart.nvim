09d1d8ef734b486c19d8be4f8954f279bbb60592
nibon7 <nibon7@163.com>
2026-04-24 21:27:06 +0800

    feat(snacks): disable dashboard and explorer

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -8,8 +8,8 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
-    explorer = { enabled = true },
+    dashboard = { enabled = false },
+    explorer = { enabled = false },
     indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
